21b28ff4a118d235e4b30568071072e5bf8f8ba8
Przemyslaw <przemyslaw.skoczewski@gmail.com>
2020-01-17 12:34:58 +0100

    fgh

--- src/main/java/pl/edu/wszib/Main.java
+++ src/main/java/pl/edu/wszib/Main.java
@@ -8,20 +8,5 @@ public class Main {
 
         UserInterface userInterface = new UserInterface();
         userInterface.ShowInterface();
-
-        //Car car = new Car(20, "Audi", "A4", 1999);
-
-
-        //CarServiceImpl.saveCar(car);
-        //CarRepository.persistCar(car);
-
-        /*Car car2 = CarRepository.getCarById(1);
-        car2.setModel("6");
-        CarRepository.persistCar(car2);
-        Car updatedCarFromDb = CarRepository.getCarById(1);
-        System.out.println(updatedCarFromDb);*/
-        //System.out.println(product);
-
-
     }
 }
